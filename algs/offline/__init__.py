196c15ab4b73e65766f1af1e5c4c324bccee1744
zaterval <ziang_zheng@foxmail.com>
2024-09-06 20:59:30 +0800

    add prifc

--- algs/offline/__init__.py
+++ algs/offline/__init__.py
@@ -3,3 +3,4 @@ from algs.offline.prdc import PRDC
 from algs.offline.augcdc import AugCDC
 from algs.offline.fzdc import FZDC
 from algs.offline.prwic import PRWIC
+from algs.offline.prifc import PRIFC
